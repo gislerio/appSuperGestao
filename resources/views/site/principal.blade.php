487045a7e4315f4cb51ab46169b0184c8dbb12b3
Gislerio Junior <gisleriojunior@hotmail.com>
2022-07-03 21:22:23 -0300

    Blade - components, token, formulário e include

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,22 +1,7 @@
 @extends('site.layouts.basico')
+@section('titulo', 'Home')
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="{{ 'img/logo.png' }}">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
-
     <div class="conteudo-destaque">
-
         <div class="esquerda">
             <div class="informacoes">
                 <h1>Sistema Super Gestão</h1>
@@ -36,30 +21,14 @@
                 <img src="{{ 'img/player_video.jpg' }}">
             </div>
         </div>
-
         <div class="direita">
             <div class="contato">
                 <h1>Contato</h1>
-                <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.
                 <p>
-                <form>
-                    <input type="text" placeholder="Nome" class="borda-branca">
-                    <br>
-                    <input type="text" placeholder="Telefone" class="borda-branca">
-                    <br>
-                    <input type="text" placeholder="E-mail" class="borda-branca">
-                    <br>
-                    <select class="borda-branca">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="">Dúvida</option>
-                        <option value="">Elogio</option>
-                        <option value="">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea class="borda-branca">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-branca">ENVIAR</button>
-                </form>
+                    Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.
+                </p>
+                @component('site.layouts._components.form_contato', ['classe'=> 'borda-branca'])
+                @endcomponent
             </div>
         </div>
     </div>
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,32 +1,20 @@
 @extends('site.layouts.basico')
+@section('titulo', 'Sobre Nós')
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="{{ 'img/logo.png' }}">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
-
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
             <h1>Olá, eu sou o Super Gestão</h1>
         </div>
-
         <div class="informacao-pagina">
-            <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios
-                da sua empresa.</p>
-            <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
+            <p>
+                O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os
+                negócios da sua empresa.
+            </p>
+            <p>
+                Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!
+            </p>
         </div>
     </div>
-
     <div class="rodape">
         <div class="redes-sociais">
             <h2>Redes sociais</h2>
