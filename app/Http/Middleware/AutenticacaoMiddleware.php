e9672a513493e2ad0c31da976fc9aa5bb139f4f6
Gislerio Junior <gisleriojunior@hotmail.com>
2022-07-18 00:06:42 -0300

    autenticação simples, sem nível de segurança. Apenas teste em laboratório

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,7 +13,7 @@ class AutenticacaoMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $metodo_autenticacao)
     {
         return $next($request);
     }
